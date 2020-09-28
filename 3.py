1b29ac7efe81d6cee81db68b646db5ca24b18995
Igor <rek771@gmail.com>
2020-09-28 23:47:34 +0300

    Revert "Сделано ДЗ к 1 занятию"
    This reverts commit d39ebfe3493c91a9e6026d0be2b5557791a67b16.

--- 3.py
+++
@@ -1,6 +0,0 @@
-# 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3.
-# Считаем 3 + 33 + 333 = 369.
-
-number = input('Введите число n: ')
-
-print(f'{number} + {number + number} + {number + number} = {int(number) + int(number + number) + int(number + number + number)}')
