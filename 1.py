af8d510701aee93e2fba565a5ade4248629131b7
Igor <rek771@gmail.com>
2020-10-08 22:04:11 +0300

    Домашка к 4 уроку

--- 1.py
+++ 1.py
@@ -1,14 +1,14 @@
-# 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление. Числа запрашивать у
-# пользователя, предусмотреть обработку ситуации деления на ноль.
+# 1. Реализовать скрипт, в котором должна быть предусмотрена функция расчета заработной платы сотрудника. В расчете
+# необходимо использовать формулу: (выработка в часах * ставка в час) + премия. Для выполнения расчета для конкретных
+# значений необходимо запускать скрипт с параметрами.
 
-def divide(num1, num2):
-    try:
-        return num1 / num2
-    except ZeroDivisionError:
-        return 'Деление на ноль'
+import argparse
 
+parser = argparse.ArgumentParser(description='(выработка в часах * ставка в час) + премия')
+parser.add_argument('-hours', dest='hours', action="store", help='Выработка в часах', type=int)
+parser.add_argument('-rate', dest='rate', action="store", help='Ставка в час', type=int)
+parser.add_argument('-bonus', dest='bonus', action="store", help='Премия', type=int)
 
-num1 = int(input('Введите число 1: '))
-num2 = int(input('Введите число 2: '))
+args = parser.parse_args()
 
-print(divide(num1, num2))
+print(f'Зарплата сотрудника {(args.hours * args.rate) + args.bonus}')
