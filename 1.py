6656bf88d4423e81fe2950872c298fc9a11346c4
Igor <rek771@gmail.com>
2020-10-13 00:02:33 +0300

    Домашка 5 урок

--- 1.py
+++ 1.py
@@ -1,14 +1,10 @@
-# 1. Реализовать скрипт, в котором должна быть предусмотрена функция расчета заработной платы сотрудника. В расчете
-# необходимо использовать формулу: (выработка в часах * ставка в час) + премия. Для выполнения расчета для конкретных
-# значений необходимо запускать скрипт с параметрами.
+# 1. Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
+# Об окончании ввода данных свидетельствует пустая строка.
 
-import argparse
-
-parser = argparse.ArgumentParser(description='(выработка в часах * ставка в час) + премия')
-parser.add_argument('-hours', dest='hours', action="store", help='Выработка в часах', type=int)
-parser.add_argument('-rate', dest='rate', action="store", help='Ставка в час', type=int)
-parser.add_argument('-bonus', dest='bonus', action="store", help='Премия', type=int)
-
-args = parser.parse_args()
-
-print(f'Зарплата сотрудника {(args.hours * args.rate) + args.bonus}')
+with open("1.txt", "a") as file:
+    while True:
+        new_str = str(input('Введите строчку: '))
+        if new_str == '':
+            break
+        else:
+            file.write(new_str + '\n')
